a503197d4df3f5f03220db62a62e522cb32125f1
L3IV <1L3IV@proton.me>
2025-10-24 10:41:06 +0200

    <MESSAGE>

---
+++ Übungsaufgabe/Lennart.java
@@ -0,0 +1,17 @@
+package Übungsaufgabe;
+
+public class Lennart {
+	
+	
+	/*
+	 * Entrance to program
+	 */
+	public static void main(String args[]) {
+		
+		String text = "Leben";
+		if (args.length==1)
+			System.out.println(args[0]);
+		System.out.println(text);
+	}
+	
+}

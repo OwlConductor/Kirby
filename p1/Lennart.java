fe0ac5efb1c6ff851a697118182eaad17befbfcd
OwlConductor <esser.lennart@smail.uni-koeln.de>
2025-10-30 21:38:42 +0100

    Short Class remove of not necessary code

--- p1/Lennart.java
+++ p1/Lennart.java
@@ -1,4 +1,4 @@
-package Übungsaufgabe1;
+package p1;
 
 public class Lennart {
 	
@@ -6,28 +6,6 @@ public class Lennart {
 	 * Entrance to program
 	 */
 	public static void main(String args[]) {
-		
-		/*
-		 * Check for input parameters
-		 */
-		if(args.length == 0) {
-			System.out.println("no parameters.");
-		} else {
-
-			// Try catch for irrigular input
-			try {
-
-				/*
-				 * looping through input pararameters and displaying them on cli
-				 */
-				for (int i = 0; i < args.length; i++)
-					System.out.println("Parameter " + i + ": " + args[i]);
-
-			} catch (Exception e) {
-				System.out.println(e.toString());
-			}
-		}
-		
 		System.out.println("Leben");
 	}
 }
